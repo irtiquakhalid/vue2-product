2c5fca631a6bdf71f43671d9f309f7c7ecb01576
irtiqua-khalid <33150303+irtiquakhalid@users.noreply.github.com>
2025-04-11 17:01:22 +0500

    initial-commit

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import Product from "./Product/index";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {
+    Product,
+  },
+});
